33a203fde42324bb4c63d9ead7038094aa438da8
venya krutikov <venya@thekazimier.co.uk>
2021-08-23 12:58:41 +0100

    Initial commit

---
+++ RGB_SENSOR_V1.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+/* Example code for the Adafruit TCS34725 breakout library */
+
+/* Connect SCL    to analog 5
+   Connect SDA    to analog 4
+   Connect VDD    to 3.3V DC
+   Connect GROUND to common ground */
+   
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup(void) {
+  Serial.begin(9600);
+  
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+  
+  // Now we're ready to get readings!
+}
+
+void loop(void) {
+  uint16_t r, g, b, c, colorTemp, lux;
+  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+  
+//  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+//  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+//  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+//  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+//  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+//  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+//  Serial.println(" ");
+
+  if ( g>=450 ){                        
+  Serial.println("GREEN");  
+  delay(100);
+  }
+  
+  if ( r>=450 ){                         
+    Serial.println("RED");
+    delay(100);
+  }
+
+  if ( b>=270 ){                         
+  Serial.println("BLUE");
+  delay(100);
+  }
+  
+  if ( b>=270 && r>=400 && g>=500 ){                        
+    Serial.println("WHITE");
+}
+}
