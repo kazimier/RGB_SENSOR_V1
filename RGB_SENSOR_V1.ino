2ada324e022bacc779ae557895f88781eaed0ec4
cpethica <charles@pethica.net>
2022-01-20 19:23:30 +0000

    changed osc messages

--- RGB_SENSOR_V1.ino
+++ RGB_SENSOR_V1.ino
@@ -203,7 +203,8 @@ void findColour(int r, int g, int b, int count) {
     
     if (nr > 0.4 && ng< 0.33) {
       if (states[count] != 1) {       // check previous state of hole and update if its now red
-        sendOSC("red", count);
+        String x = "red"+count;
+        sendOSC(x,0);
         states[count] = 1;        
         for(int i=1; i<4; i++) {        // set neopixel colour
           pixels.setPixelColor(i+(count-4)*3, pixels.Color(100, 0, 0));
@@ -213,7 +214,8 @@ void findColour(int r, int g, int b, int count) {
     }
     if (ng > 0.38 && nr < 0.33 && nb < 0.29) {
       if (states[count] != 2) {       // check previous state of hole and update if its a new colour            
-        sendOSC("green", count);
+        String y = "green"+count;
+        sendOSC(y,0);
         states[count] = 2;    
         for(int i=1; i<4; i++) {        // set neopixel colour
           pixels.setPixelColor(i+(count-4)*3, pixels.Color(0, 100, 0));
@@ -223,7 +225,8 @@ void findColour(int r, int g, int b, int count) {
     }
     if (nb > 0.34 && nr < 0.3) {
       if (states[count] != 3) {       // check previous state of hole and update if its a new colour         
-        sendOSC("blue", count); 
+        String z = "blue"+count;        
+        sendOSC(z, 0); 
         states[count] = 3;      
         for(int i=1; i<4; i++) {        // set neopixel colour
           pixels.setPixelColor(i+(count-4)*3, pixels.Color(0, 0, 100));
@@ -233,7 +236,8 @@ void findColour(int r, int g, int b, int count) {
     } 
     if (nr > 0.35 && nb < 0.27 && ng > 0.35) {
       if (states[count] != 4) {       // check previous state of hole and update if its a new colour  
-        sendOSC("yellow", count);
+        String a = "yellow"+count;        
+        sendOSC(a, 0);
         states[count] = 4;  
         for(int i=1; i<4; i++) {        // set neopixel colour
           pixels.setPixelColor(i+(count-4)*3, pixels.Color(81, 80, 0));
